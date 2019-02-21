b4b2c54acc85d01df8f24256483e6d548d15b0b7
Joshua Whitley <jwhitley@autonomoustuff.com>
2019-02-21 13:44:00 -0600

    socketcan_interface passes all tests!

--- socketcan_interface/test/test_string.cpp
+++ socketcan_interface/test/test_string.cpp
@@ -35,7 +35,7 @@ TEST(StringTest, stringconversion)
 }
 
 // Run all the tests that were declared with TEST()
-int main(int argc, char **argv)
+int main(int argc, char ** argv)
 {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
